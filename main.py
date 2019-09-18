316780ff0695a9f6d1af9872090917ed50ba5ef7
Bertil <ipsen.bertil@gmail.com>
2019-09-18 16:32:17 +0200

    bunch of changes, merged all somes to one for loop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Scrapes IBM data"""
-
+# TODO: facebook scraper doesnt download the correct image
 
 import logging
 import argparse
@@ -36,7 +36,7 @@ def download_twitter_photo(tweet_url):
         img_url = img.get('data-image-url')
         filename = tweet_url.split('.com/')[1].replace('/', '-') + '.jpg'
         urlretrieve(img_url, f'tweet_imgs/{filename}')
-    except:
+    except: facebook
         TWITTER_LOGGER.exception(f'{tweet_url} was not downloaded')
 
 
@@ -45,10 +45,24 @@ def download_instagram_photo(instagram_post_url):
     try:
         shortcode = instagram_post_url.split('/')[-2]
         post = Post.from_shortcode(L.context, shortcode)
-        L.download_post(post=post, target='posts')
+        L.download_post(post=post, target='instagram_imgs')
     except:
         INSTAGRAM_LOGGER.exception(f'{instagram_post_url} was not downloaded')
         pass
+
+
+def download_facebook_photo(facebook_post_url):
+    """Downloads photos from Facebook"""
+    facebook_html = urlopen(facebook_post_url)
+    soup = BeautifulSoup(facebook_html, 'lxml')
+    img = soup.find(
+        "img", {"class": "scaledImageFitWidth img"})
+    img_url = img.get('src')
+    filename = facebook_post_url.split('.com/')[1].replace('/', '-') + '.jpg'
+    urlretrieve(img_url, f'facebook_imgs/{filename}')
+    with open('facebook_name_log.csv', 'w') as log_file:
+        facebook_writer = csv.writer(log_file)
+        facebook_writer.writerow([img_url, filename])
 
 
 # Set up argparse
@@ -63,15 +77,20 @@ INSTAGRAM_LOGGER = setup_logger('instagram_logger', 'instagram_logfile.log')
 # Read data
 DATA = pd.read_csv(ARGS.datafile, low_memory=False)
 
-# Twitter scrape
-print('Scraping Twitter...')
-TWITTER_DATA = DATA[DATA['SocialNetwork'] == 'TWITTER']
-for tweet in tqdm(TWITTER_DATA['Permalink']):
-    download_twitter_photo(tweet)
-
-# Instagram scrape
+# Initialize Instaloader
 L = instaloader.Instaloader()
-print('Scraping Instagram...')
-INSTAGRAM_DATA = DATA[DATA['SocialNetwork'] == 'INSTAGRAM']
-for instagram_post in INSTAGRAM_DATA['Permalink']:
-    download_instagram_photo(instagram_post)
+
+social_medias = ['FACEBOOK', 'TWITTER', 'INSTAGRAM']
+
+for so_me in social_medias:
+    print(f'Scraping {so_me}...')
+    links = DATA[DATA['SocialNetwork'] == so_me]['Permalink']
+    if not os.path.exists(f'{so_me}_imgs'):
+        os.mkdir({f'{so_me}_imgs')
+        for url in tqdm(links):
+            if so_me == 'FACEBOOK':
+                 download_facebook_photo(url)
+            if so_me == 'TWITTER':
+                 download_twitter_photo(url)
+            if so_me == 'INSTAGRAM':
+                 download_instagram_photo(url)
