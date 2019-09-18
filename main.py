b784e8744cf28989743e6dd3ddd95fd111d21eb6
Bertil <ipsen.bertil@gmail.com>
2019-09-18 16:46:21 +0200

    minor changes

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def setup_logger(name, log_file, level=logging.INFO):
     return logger
 
 
-def download_twitter_photo(tweet_url):
+def download_twitter_photo(tweet_url, photo_folder):
     """Downloads photos from Twitter"""
     try:
         tweet_html = urlopen(tweet_url)
@@ -35,23 +35,23 @@ def download_twitter_photo(tweet_url):
             "div", {"class": "AdaptiveMedia-photoContainer js-adaptive-photo"})
         img_url = img.get('data-image-url')
         filename = tweet_url.split('.com/')[1].replace('/', '-') + '.jpg'
-        urlretrieve(img_url, f'tweet_imgs/{filename}')
+        urlretrieve(img_url, photo_folder)
     except: facebook
         TWITTER_LOGGER.exception(f'{tweet_url} was not downloaded')
 
 
-def download_instagram_photo(instagram_post_url):
+def download_instagram_photo(instagram_post_url, photo_folder):
     """Downloads photos from Instagram"""
     try:
         shortcode = instagram_post_url.split('/')[-2]
         post = Post.from_shortcode(L.context, shortcode)
-        L.download_post(post=post, target='instagram_imgs')
+        L.download_post(post=post, target='photo_folder')
     except:
         INSTAGRAM_LOGGER.exception(f'{instagram_post_url} was not downloaded')
         pass
 
 
-def download_facebook_photo(facebook_post_url):
+def download_facebook_photo(facebook_post_url, photo_folder):
     """Downloads photos from Facebook"""
     facebook_html = urlopen(facebook_post_url)
     soup = BeautifulSoup(facebook_html, 'lxml')
@@ -59,7 +59,7 @@ def download_facebook_photo(facebook_post_url):
         "img", {"class": "scaledImageFitWidth img"})
     img_url = img.get('src')
     filename = facebook_post_url.split('.com/')[1].replace('/', '-') + '.jpg'
-    urlretrieve(img_url, f'facebook_imgs/{filename}')
+    urlretrieve(img_url, photo_folder)
     with open('facebook_name_log.csv', 'w') as log_file:
         facebook_writer = csv.writer(log_file)
         facebook_writer.writerow([img_url, filename])
@@ -80,17 +80,24 @@ DATA = pd.read_csv(ARGS.datafile, low_memory=False)
 # Initialize Instaloader
 L = instaloader.Instaloader()
 
+# Scrape images from different SoMe
 social_medias = ['FACEBOOK', 'TWITTER', 'INSTAGRAM']
-
 for so_me in social_medias:
     print(f'Scraping {so_me}...')
+
+    # Get the post urls for each SoMe
     links = DATA[DATA['SocialNetwork'] == so_me]['Permalink']
-    if not os.path.exists(f'{so_me}_imgs'):
-        os.mkdir({f'{so_me}_imgs')
-        for url in tqdm(links):
-            if so_me == 'FACEBOOK':
-                 download_facebook_photo(url)
-            if so_me == 'TWITTER':
-                 download_twitter_photo(url)
-            if so_me == 'INSTAGRAM':
-                 download_instagram_photo(url)
+
+    # Make a folder to put photos for each SoMe
+    photo_folder = f'photos/{so_me}_imgs')
+    if not os.path.exists(photo_folder):
+        os.mkdir(photo_folder)
+
+    # Scrape photos
+    for url in tqdm(links):
+        if so_me == 'FACEBOOK':
+            download_facebook_photo(url, photo_folder)
+        if so_me == 'TWITTER':
+            download_twitter_photo(url, photo_folder)
+        if so_me == 'INSTAGRAM':
+            download_instagram_photo(url, photo_folder)
