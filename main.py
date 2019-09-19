ec8c728cbb4ec849715d7a0253d2e4c5f5da7728
Bertil <ipsen.bertil@gmail.com>
2019-09-19 14:06:39 +0200

    twitter and instagram works. updated to pathlib

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 TODO: facebook scraper doesnt download the correct image
 """
 
-import os
 import logging
+import csv
 import argparse
+from pathlib import Path
 from urllib.request import urlopen, urlretrieve
 import pandas as pd
 from bs4 import BeautifulSoup
 import instaloader
 from tqdm import tqdm
+
+
+def download_facebook_photo(url, photo_folder):
+    pass
 
 
 def download_twitter_photo(tweet_url, tweet_photo_folder):
@@ -21,13 +26,14 @@ def download_twitter_photo(tweet_url, tweet_photo_folder):
         soup = BeautifulSoup(tweet_html, 'lxml')
 
         # Find image using platform specific tag
-        img = soup.find("div", {"class": 'AdaptiveMedia-photoContainer js-adaptive-photo')
+        img = soup.find(
+            "div", {"class": 'AdaptiveMedia-photoContainer js-adaptive-photo'})
 
         # Get image url using platform specific tag
         img_url = img.get('data-image-url')
 
         # Urls aren't allowed in filenames, so we change / to -
-        filename = post_url.split('.com/')[1].replace('/', '-') + '.jpg'
+        filename = tweet_url.split('.com/')[1].replace('/', '-') + '.jpg'
 
         # Keep a log of changed filenames, for each social media
         with open(f'twitter_name_log.csv', 'w') as log_file:
@@ -35,11 +41,11 @@ def download_twitter_photo(tweet_url, tweet_photo_folder):
             writer.writerow([img_url, filename])
 
         # Download photo
-        filepath = os.path.join(tweet_photo_folder, filename)
+        filepath = Path(tweet_photo_folder) / filename
         urlretrieve(img_url, filepath)
 
-    except:
-        logging.exception(f'{post_url} was not downloaded from {media}')
+    except Exception as e:
+        logging.exception(f'{tweet_url} was not downloaded from Twitter')
 
 
 def download_instagram_photo(instagram_post_url, instagram_photo_folder):
@@ -48,7 +54,7 @@ def download_instagram_photo(instagram_post_url, instagram_photo_folder):
         # Shortcode is the last part of instagram post url
         shortcode = instagram_post_url.split('/')[-2]
         post = instaloader.Post.from_shortcode(L.context, shortcode)
-        L.download_post(post=post, target=instagram_photo_folder)
+        L.download_post(post=post, target=Path(instagram_photo_folder))
     except:
         logging.exception(
             f'{instagram_post_url} was not downloaded from Instagram')
@@ -78,13 +84,12 @@ for so_me in SOCIAL_MEDIA:
     links = DATA[DATA['SocialNetwork'] == so_me]['Permalink']
 
     # Make a folder to put photos for each SoMe
-    photo_folder = f'photos/{so_me}_imgs'
-    if not os.path.exists(photo_folder):
-        os.mkdir(photo_folder)
+    photo_folder = Path('photos') / f'{so_me}_imgs'
+    Path.mkdir(photo_folder, parents=True, exist_ok=True)
 
     # Scrape photos
+    func_dict = {'FACEBOOK': download_facebook_photo,
+                 'TWITTER': download_twitter_photo,
+                 'INSTAGRAM': download_instagram_photo}
     for url in tqdm(links):
-        if so_me == 'TWITTER':
-            download_twitter_photo(url, photo_folder)
-        if so_me == 'INSTAGRAM':
-            download_instagram_photo(url, photo_folder)
+        func_dict[so_me](url, photo_folder)
